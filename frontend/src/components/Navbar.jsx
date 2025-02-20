360379fa78d6e3b1c789ddc2c3e7eeeb1b2a2acb
Tutul Kumar Biswas <tutulkumar23@gmail.com>
2025-02-20 10:57:03 +0600

    profile and cart

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import search from "../assets/search.png";
+import profile from "../assets/profile.png";
+import cart_icon from "../assets/cart_icon.png";
 const Navbar = () => {
   return (
     <div className="flex  items-center justify-between  py-5  font-medium">
@@ -29,6 +31,21 @@ const Navbar = () => {
 
       <div className="flex item-center gap-6">
         <img src={search} alt="" className="w-5 cursor-pointer" />
+        <div className="group relative">
+          <img className="w-6 cursor-pointer" src={profile} alt="" />
+          <div className="group-hover:block hidden absolute right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        <Link to="/cart" className="relative">
+          <img src={cart_icon} alt="" className="w-6 min-w-6 cursor-pointer" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leadind-4 bg-black text-white aspect-square text-[8px] rounded-full">10</p>
+        </Link>
+
       </div>
     </div>
   );
