178551d6ff49270a115e3c8232bad48a421e740f
mhbarshan <mhb247@gmail.com>
2025-03-23 22:45:21 +0600

    Admin dashboard developed

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+const Navbar = ({ setToken }) => {
+  return (
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      <img className="w-[max(5%,20px)] " src={logo} alt="" />
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600  text-white px-4 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
