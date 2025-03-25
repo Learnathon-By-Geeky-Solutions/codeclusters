307de7c8b360d99995adda34eff1b9429b57a249
mhbarshan <64357462+mhbarshan@users.noreply.github.com>
2025-03-26 02:40:16 +0600

    Delete backend directory

--- backend/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addReviews, getReviews } from "../controllers/reviewController.js";
-import userAuth from "../middleware/userAuth.js";
-const reviewRouter = express.Router();
-
-reviewRouter.get("/allReview", getReviews);
-reviewRouter.post("/addReview", userAuth, addReviews);
-
-export default reviewRouter;
