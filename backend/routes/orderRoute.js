8d776798bd05a47533252e598885a136935906f0
mhbarshan <mhb247@gmail.com>
2025-04-10 01:21:11 +0600

    solved sonar error

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,4 +1,5 @@
 import express from "express";
+import RateLimit from "express-rate-limit";
 import {
   allOrders,
   placeOrder,
@@ -12,6 +13,12 @@ import userAuth from "../middleware/userAuth.js";
 
 const orderRouter = express.Router();
 
+// Rate limiter: maximum of 100 requests per 15 minutes
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
 //admin features
 orderRouter.post("/list", adminAuth, allOrders);
 orderRouter.post("/status", adminAuth, updateStatus);
@@ -24,6 +31,6 @@ orderRouter.post("/stripe", userAuth, placeOrderStripe);
 orderRouter.post("/userorders", userAuth, userOrders);
 
 //verifyPayment
-orderRouter.post("/verifyStripe", userAuth, verifyStripe);
+orderRouter.post("/verifyStripe", userAuth, limiter, verifyStripe);
 
 export default orderRouter;
