b40af2f3e01354fd5dbcc9561283e8577b8c0c03
mhbarshan <mhb247@gmail.com>
2025-04-12 00:52:03 +0600

    Fix routing for SPA in admin

---
+++ admin/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../assets/logo.png";
+const Home = () => {
+  return (
+    <div className="flex justify-center flex-col items-center w-full">
+      <h1>Welcome Admin!</h1>
+      <img className="h-64 w-64" src={logo} alt="" />
+    </div>
+  );
+};
+
+export default Home;
