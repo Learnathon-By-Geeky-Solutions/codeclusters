13b648a0cfb1f86864fffae8823278214022150c
mhbarshan <mhb247@gmail.com>
2025-04-15 21:02:39 +0600

    resolved conflict

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -9,10 +9,9 @@ import userAuth from "../middleware/userAuth.js";
 
 const cartRouter = express.Router();
 
-// set up rate limiter: maximum of 100 requests per 15 minutes
 const limiter = RateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 100 requests per windowMs
+  windowMs: 15 * 60 * 1000,
+  max: 100,
 });
 
 cartRouter.post("/get", userAuth, getUserCart);
