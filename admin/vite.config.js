2af0c76261b8dde06ca5b1098b03f471e6fbca7d
mhbarshan <mhb247@gmail.com>
2025-04-22 09:42:51 +0600

    added coverage into admin for test

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     setupFiles: "./src/setupTests.js",
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "html", "lcov"],
     },
   },
 });
